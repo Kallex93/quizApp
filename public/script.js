df1fe2fdd959ac1628c7715cd47885f4e8f58776
Kallex93 <alessio.vaccarini@gmail.com>
2022-09-06 00:30:01 +0200

    many updates

--- public/script.js
+++ public/script.js
@@ -1,16 +1,71 @@
+const startBtn = document.getElementById('start-btn');
+const answerButtonsContainer = document.getElementById('answer-buttons');
+const questionText = document.getElementById('question-text');
 
-function show(){
+
+var shuffledQuestions, currentIndex;
+
+if (startBtn) {
+    startBtn.addEventListener('click', startGame);
+}
+
+async function startGame(){
+    console.log("Game Started");
+
+    startBtn.classList.add('hide');
+    answerButtonsContainer.classList.remove('hide');
+
+    var question_list;
+    currentIndex = 0;
+    
+    //LOAD QUESTIONS FILE
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             // Typical action to be performed when the document is ready:
-            var data = JSON.parse(xhttp.response);
-            console.log(data);
+            var data = JSON.parse(xhttp.responseText);
+            
+            console.log("Received (" + (typeof data) + ")" + data);
+            question_list = Object.values(data.questions);
+            shuffledQuestions = question_list.sort((a, b) => 0.5 - Math.random());
+            console.log(question_list);
+            nextQuestion();
+        }
+    };
 
+    xhttp.open("GET", "questions.json", true);
+    xhttp.send();
+
+}
+
+function nextQuestion(){
+    showNextQuestion(shuffledQuestions[currentIndex]);
+}
+
+function showNextQuestion(question){
+    questionText.innerText = question.question_text;
+}
+
+function selectAnswer(){
+
+}
+
+
+function loadQuestions(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Typical action to be performed when the document is ready:
+            var data = xhttp.response;
+            
+            console.log("Received: " + data);
+            /*
+            console.log(data);
             var questions = data.questions;
-            
             document.getElementById('question-text').innerHTML = questions[0].question_text;
-            
+            */
+
+            return data;
         
         }
     };
@@ -18,6 +73,6 @@ function show(){
     xhttp.send();
 }
 
-window.addEventListener('load', show);
+
 
 
