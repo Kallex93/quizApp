436940e3c8db7b629c930987e0ba15e3c7b950ef
kallex93 <alessio.vaccarini@gmail.com>
2022-09-12 17:17:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
-const port = 3000
+const port = process.env.PORT || 3000;
 const Player = require("./playerSchema");
 
 
